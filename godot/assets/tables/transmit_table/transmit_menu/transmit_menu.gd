75e0483cfcad94fbacc9be0965a89bfa408a3d9b
Timothy Mead <44774066+ti-mo-thy@users.noreply.github.com>
2025-09-30 15:03:33 +1000

    Update transmit table UI (#21)
    * Replace add-transmit entry menu with a add-row button that is live editable
    
    * Remove print
    
    * Rearrange send table button
    
    * Fix transmit table alignment and headers
    
    * Fix hex byte conversion and add error checking
    
    * Add edit checks and bounds on data being entered into transmit table. Add auto formatting/byte seperation of transmit data. Fix hex decode issue.
    
    * Add data placeholder text
    
    * Update dbc to add ack
    
    ---------
    
    Co-authored-by: Timothy Mead <t.mead@cyborgdynamicseng.com>
    Co-authored-by: Justin Beri <j.beri@cyborgdynamicseng.com>

--- godot/assets/tables/transmit_table/transmit_menu/transmit_menu.gd
+++
@@ -1,6 +0,0 @@
-extends Window
-class_name TransmitMenu
-
-func _ready() -> void:
-    self.close_requested.connect(self.hide)
-    self.hide()
--- godot/assets/tables/transmit_table/transmit_table.gd
+++ godot/assets/tables/transmit_table/transmit_table.gd
@@ -5,6 +5,7 @@ class_name TransmitTable
 @onready var table_row = preload("res://assets/tables/table_row.tscn")
 @onready var table_cell = preload("res://assets/tables/table_cell.tscn")
 @onready var table_button = preload("res://assets/tables/table_button.tscn")
+@onready var table_send_text_cell = preload("res://assets/tables/transmit_table/send_text_cell.tscn")
 @onready var table_send_check_box = preload("res://assets/tables/transmit_table/send_checkbox_cell.tscn")
 @onready var table_send_delete_button = preload("res://assets/tables/transmit_table/send_delete_cell.tscn")
 @onready var can_format_button: CanFormatButton = get_tree().current_scene.get_node("Background/TabContainer/Interface/CanFormatButton")
@@ -17,10 +18,10 @@ const DELETE_IDX = 0
 const TOGGLE_IDX = 1
 const CYCLE_TIME_IDX = 2
 const CAN_ID_IDX = 3
-const DATA_START_IDX = 4
+const DATA_IDX = 4
 
 const CELL_HEIGHT = 25
-const CELL_WIDTHS = [25, 25, 120, 100, 60]
+const CELL_WIDTHS = [60, 60, 120, 120, 250]
 
 
 func _ready() -> void:
@@ -35,39 +36,13 @@ func _physics_process(_delta: float) -> void:
 			send_entry.send_if_ready()
 
 
-func add_new_send_entry(can_id: int, extended_id: bool, cycle_time_ms: int, data: Array) -> void:
-	# Check bounds of CAN ID
-	if extended_id:
-		const EXTENDED_CAN_ID_MIN: int = 0
-		const EXTENDED_CAN_ID_MAX: int = 536870911
-		if can_id < EXTENDED_CAN_ID_MIN or can_id > EXTENDED_CAN_ID_MAX:
-			printerr("CAN ID of " + str(can_id) + " is invalid for Extended CAN messages")
-			return
-	else:
-		const STANDARD_CAN_ID_MIN: int = 0
-		const STANDARD_CAN_ID_MAX: int = 2047
-		if can_id < STANDARD_CAN_ID_MIN or can_id > STANDARD_CAN_ID_MAX:
-			printerr("CAN ID of " + str(can_id) + " is invalid for Standard CAN messages")
-			return
-
-	# Check bounds of data elements
-	const BYTE_MAX_VAL: int = 255
-	for byte: int in data:
-		if byte < 0:
-			printerr("Attempt to pass a negative value to a CAN frame (" + str(byte) + "). Please use the two's complement if this is intentional")
-			return
-		if byte > BYTE_MAX_VAL:
-			printerr("Data value of " + str(byte) + " cannot fit into a single byte")
-			return
-
-	var send_entry := TransmitTableEntry.new(godot_can_bridge, can_id, extended_id, cycle_time_ms, data)
-	send_entries.append(send_entry)
-	_generate_new_data_row(send_entry)
+func add_new_send_entry() -> void:
+	send_entries.append(TransmitTableEntry.new(godot_can_bridge, self))
 
 
 # Adds the header row to the table, should only be called once
 func _generate_header_row() -> void:
-	var header = ["", "", "Cycle Time [ms]", "CAN ID", "Data"]
+	var header = ["Delete", "Send", "Cycle Time [ms]", "CAN ID [hex]", "Data [hex]"]
 	var header_row: BoxContainer = table_row.instantiate()
 
 	for i in range(len(header)):
@@ -75,7 +50,7 @@ func _generate_header_row() -> void:
 		cell.custom_minimum_size = Vector2(CELL_WIDTHS[i], CELL_HEIGHT)
 
 		# Set the data row to fill all remaining horizontal space
-		if i == DATA_START_IDX:
+		if i == DATA_IDX:
 			cell.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 		
 		_update_label_and_font_size(cell.get_node("Label"), header[i], CELL_WIDTHS[i])
@@ -83,51 +58,6 @@ func _generate_header_row() -> void:
 		header_row.add_child(cell)
 	
 	rows.add_child(header_row)
-
-
-# Adds a new data row to the table, should only be called once for each new unique CAN_ID received
-func _generate_new_data_row(send_entry: TransmitTableEntry):
-	var row: BoxContainer = table_row.instantiate()
-	rows.add_child(row)
-
-	# Add delete button
-	var delete_cell: PanelContainer = table_send_delete_button.instantiate()
-	delete_cell.custom_minimum_size = Vector2(CELL_WIDTHS[DELETE_IDX], CELL_HEIGHT)
-	row.add_child(delete_cell)
-
-	# Connect the underlying button object to a lambda that deletes this send entry and GUI row
-	var delete_button: Button = delete_cell.get_node("SendButton")
-	delete_button.pressed.connect(
-		func():
-			send_entries.erase(send_entry)
-			row.queue_free()
-	)
-
-	# Add check box
-	var check_box_cell: PanelContainer = table_send_check_box.instantiate()
-	check_box_cell.custom_minimum_size = Vector2(CELL_WIDTHS[TOGGLE_IDX], CELL_HEIGHT)
-	row.add_child(check_box_cell)
-	send_entry.set_checkbox(check_box_cell.get_node("CheckBox")) # Link check box to the send entry to allow the entry to check whether it should be sending
-
-	# Add labels
-	var labels := send_entry.get_labels(self)
-	for label_idx in len(labels):
-		# When adding the labels, the delete and toggle cells are skipped (the first label is the cycle_time_ms)
-		var cell_idx := label_idx + CYCLE_TIME_IDX
-
-		var cell: PanelContainer = table_cell.instantiate()
-		var cell_width = CELL_WIDTHS[cell_idx] if cell_idx < DATA_START_IDX else CELL_WIDTHS[DATA_START_IDX]
-		cell.custom_minimum_size = Vector2(cell_width, CELL_HEIGHT)
-
-		var label := labels[label_idx]
-
-		# Ensure CAN ID labels are right aligned
-		if cell_idx == CAN_ID_IDX:
-			cell.get_node("Label").horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
-		
-		_update_label_and_font_size(cell.get_node("Label"), label, cell_width)
-
-		row.add_child(cell)
 
 
 # Updates the text and tooltip of a label, making adjustments to size and concatonating the label if neccesary
@@ -153,76 +83,205 @@ func _update_label_and_font_size(label, new_text: String, width: int) -> void:
 
 
 func update_formatting() -> void:
-	for entry_idx in range(len(send_entries)):
-		var send_entry: TransmitTableEntry = send_entries[entry_idx]
-		var entry_row: Node = rows.get_child(1 + entry_idx) # Skip the first child which is the header row
-		var entry_cells: Array = entry_row.get_children()
-		var updated_labels: Array = send_entry.get_labels(self)
-
-		# Only reformat CAN ID cell and onwards
-		for cell_idx in range(CAN_ID_IDX, len(entry_cells)):
-			var cell_width = CELL_WIDTHS[cell_idx] if cell_idx < DATA_START_IDX else CELL_WIDTHS[DATA_START_IDX]
-			_update_label_and_font_size(entry_cells[cell_idx].get_node("Label"), updated_labels[cell_idx - CYCLE_TIME_IDX], cell_width)
-
-
-func format_can_id(can_id: int, is_ext_can: bool) -> String:
-	if can_format_button.use_hex():
-		return "0x" + ("%08x" % can_id).to_upper() if is_ext_can else "0x" + ("%03x" % can_id).to_upper()
-	else:
-		return "0d" + ("%09x" % can_id).to_upper() if is_ext_can else "0d" + ("%04x" % can_id).to_upper()
-
-
-func format_can_data_byte(can_id: int) -> String:
-	# Assumes 8 bit length
-	if can_format_button.use_hex():
-		return "0x" + ("%02x" % can_id).to_upper()
-	else:
-		return "0d" + ("%03d" % can_id)
+	# Send entries only have hex mode supported right now so do nothing
+	pass
 
 
 class TransmitTableEntry:
 	var _godot_can_bridge: GodotCanBridge
-	var _can_id: int
-	var _cycle_time_ms: int
-	var _data: Array
-	var _last_send_time_ms: float
-	var _check_box: CheckBox
-	var _extended_id: bool
+	var _transmit_table: TransmitTable
 
-	func _init(godot_can_bridge: GodotCanBridge, can_id: int, extended_id: bool, cycle_time_ms: int, data: Array) -> void:
+	var _last_send_time_ms: float
+
+	var _row: BoxContainer
+	var _check_box: CheckBox
+	var _can_id_box: LineEdit
+	var _data_box: LineEdit
+	var _cycle_time_box: LineEdit
+
+
+	func _init(godot_can_bridge: GodotCanBridge, transmit_table: TransmitTable) -> void:
 		_godot_can_bridge = godot_can_bridge
-		_can_id = can_id
-		_extended_id = extended_id
-		_cycle_time_ms = cycle_time_ms
-		_data = data
+		_transmit_table = transmit_table
+
 		_last_send_time_ms = Time.get_ticks_msec()
+		_instantiate_labels()
+
+
+	# Instantiates the table cell nodes holding the labels into this entry's row
+	func _instantiate_labels():
+		_row = _transmit_table.table_row.instantiate()
+		_transmit_table.rows.add_child(_row)
+
+		# Add delete button
+		var delete_cell: PanelContainer = _transmit_table.table_send_delete_button.instantiate()
+		delete_cell.custom_minimum_size = Vector2(CELL_WIDTHS[DELETE_IDX], CELL_HEIGHT)
+		_row.add_child(delete_cell)
+
+		# Connect the underlying button object to a lambda that deletes this send entry and GUI row
+		var delete_button: Button = delete_cell.get_node("SendButton")
+		delete_button.pressed.connect(
+			func():
+				_row.queue_free()
+				_transmit_table.send_entries.erase(self)
+		)
+
+		# Add check box
+		var check_box_cell: PanelContainer = _transmit_table.table_send_check_box.instantiate()
+		check_box_cell.custom_minimum_size = Vector2(CELL_WIDTHS[TOGGLE_IDX], CELL_HEIGHT)
+		_row.add_child(check_box_cell)
+		_check_box = check_box_cell.get_node("CheckBox") # Link check box to the send entry to allow the entry to check whether it should be sending
+
+		# Add cycle time box
+		var cycle_time_cell: PanelContainer = _transmit_table.table_send_text_cell.instantiate()
+		cycle_time_cell.custom_minimum_size = Vector2(CELL_WIDTHS[CYCLE_TIME_IDX], CELL_HEIGHT)
+		_cycle_time_box = cycle_time_cell.get_node("LineEdit")
+		_transmit_table._update_label_and_font_size(_cycle_time_box, "0", 120)
+		_row.add_child(cycle_time_cell)
+
+		# Only allow numeric characters for cycle time box
+		_cycle_time_box.max_length = 4
+		_cycle_time_box.text_changed.connect(
+			func(new_text: String):
+				var filtered := ""
+				for c in new_text:
+					if c.is_valid_int():
+						filtered += c
+				
+				# Update the text with the invalid characters removed, maintaining the old cursor position
+				var old_cursor_pos := _cycle_time_box.caret_column
+				_cycle_time_box.text = filtered
+				_cycle_time_box.caret_column = min(old_cursor_pos - (new_text.length() - filtered.length()), _cycle_time_box.text.length())
+		)
+
+		# Add Can ID box
+		var can_id_cell: PanelContainer = _transmit_table.table_send_text_cell.instantiate()
+		can_id_cell.custom_minimum_size = Vector2(CELL_WIDTHS[CAN_ID_IDX], CELL_HEIGHT)
+		_can_id_box = can_id_cell.get_node("LineEdit")
+		_transmit_table._update_label_and_font_size(_can_id_box, "000", CELL_WIDTHS[CAN_ID_IDX])
+		_row.add_child(can_id_cell)
+
+		# Only allow valid hex characters for CAN ID box
+		_can_id_box.max_length = 8
+		_can_id_box.text_changed.connect(
+			func(new_text: String):
+				var filtered := ""
+				for c in new_text:
+					if c.is_valid_hex_number():
+						filtered += c
+				
+				# Update the text with the invalid characters removed, maintaining the old cursor position
+				var old_cursor_pos := _can_id_box.caret_column
+				_can_id_box.text = filtered
+				_can_id_box.caret_column = min(old_cursor_pos - (new_text.length() - filtered.length()), _can_id_box.text.length())
+		)
+
+		# Add Data box
+		var data_cell: PanelContainer = _transmit_table.table_send_text_cell.instantiate()
+		data_cell.custom_minimum_size = Vector2(CELL_WIDTHS[DATA_IDX], CELL_HEIGHT)
+		data_cell.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+		_data_box = data_cell.get_node("LineEdit")
+		_transmit_table._update_label_and_font_size(_data_box, "", CELL_WIDTHS[DATA_IDX])
+		_row.add_child(data_cell)
+
+		# Add byte seperation formatting and character validation for Data box
+		_data_box.placeholder_text = "00 00 00 00 00 00 00 00"
+		_data_box.max_length = 23
+		_data_box.text_changed.connect(
+			func(new_text: String):
+				# Filter out non-hex characters
+				var filtered := ""
+				for c in new_text:
+					if c.is_valid_hex_number():
+						filtered += c
+
+				# Add seperation spaces between every byte
+				var bytes := []
+				var start := filtered.length() % 2 # 1 if odd, 0 if even
+				if start == 1:
+					bytes.append(filtered[0]) # first lone char
+				for i in range(start, filtered.length(), 2):
+					bytes.append(filtered.substr(i, 2))
+				filtered = " ".join(bytes)
+
+				# Update the text with the invalid characters removed, maintaining the old cursor position
+				var old_cursor_pos := _data_box.caret_column
+				_data_box.text = filtered
+				_data_box.caret_column = min(old_cursor_pos - (new_text.length() - filtered.length()), _data_box.text.length())
+		)
+
+
+	func can_id() -> int:
+		return _can_id_box.text.replace(" ", "").hex_to_int()
+
+
+	func can_id_valid() -> bool:
+		return _can_id_box.text.replace(" ", "").is_valid_hex_number()
+
 
 	func is_ext_can() -> bool:
-		return _extended_id
+		const STANDARD_CAN_ID_MAX: int = 2047
+		return (can_id() > STANDARD_CAN_ID_MAX) or (_can_id_box.text.length() > 3)
+
+
+	func cycle_time_ms() -> int:
+		return int(_cycle_time_box.text)
+
 
 	# Returns true if this entry should be currently transmitting
 	func enabled() -> bool:
 		return _check_box.button_pressed
 
-	# Sets the checkbox used by the entry to check whether the entry is enabled
-	func set_checkbox(check_box: CheckBox) -> void:
-		_check_box = check_box
+
+	# Returns the payload data stored in this entry in its original hex format
+	func hex_data() -> String:
+		return _data_box.text.replace(" ", "")
+	
+
+	func hex_data_valid() -> bool:
+		return hex_data().is_empty() or hex_data().is_valid_hex_number()
+
+
+	# Returns the payload data stored in this entry converted into a byte array representation
+	func data() -> Array:
+		return hex_to_byte_array(hex_data())
+
 
 	# Sends this entry over the CAN interface if cycle time has been exceeded
 	func send_if_ready() -> void:
+		var cycle_time = cycle_time_ms()
 		var current_time_ms := Time.get_ticks_msec()
-		if (current_time_ms - _last_send_time_ms) > _cycle_time_ms:
-			_godot_can_bridge.send_can_frame(_can_id, is_ext_can(), _data)
+		if (current_time_ms - _last_send_time_ms) > cycle_time:
+			if not hex_data_valid():
+				_check_box.button_pressed = false
+				AlertHandler.display_error("Invalid hex data provided")
+				return
+			
+			if not can_id_valid():
+				_check_box.button_pressed = false
+				AlertHandler.display_error("Invalid CAN ID provided")
+				return
+
+			_godot_can_bridge.send_can_frame(can_id(), is_ext_can(), data())
 			_last_send_time_ms = current_time_ms
 		
 		# A cycle time of 0ms should be treated as 'one shot', so disable itself after sending the message
-		if _cycle_time_ms == 0:
+		if cycle_time == 0:
 			_check_box.button_pressed = false
 
-	# Returns the labels of this entry for display
-	func get_labels(table: TransmitTable) -> Array[String]:
-		var labels: Array[String] = [str(_cycle_time_ms), table.format_can_id(_can_id, is_ext_can())]
-		for byte in _data:
-			labels.append(table.format_can_data_byte(int(byte)))
 
-		return labels
+	# Converts a hex string into a u8 byte array
+	func hex_to_byte_array(hex_str: String) -> Array:
+		var bytes: Array = []
+		
+		# Ensure an even number of characters (each byte = 2 hex digits)
+		if hex_str.length() % 2 != 0:
+			hex_str = "0" + hex_str
+
+		# Walk through the string in steps of 2
+		for i in range(0, hex_str.length(), 2):
+			var byte_str := hex_str.substr(i, 2)
+			var byte_val = byte_str.hex_to_int() # parse as hex
+			bytes.append(byte_val)
+		
+		return bytes
