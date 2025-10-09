f2215ed694ce7fab8e248a57c2c85f2d05cfca65
Timothy Mead <t.mead@cyborgdynamicseng.com>
2025-10-09 16:04:22 +1000

    Fix rescaling minimum sizes of elements. Remove embedded subwindows. Add custom minimum size of the main window to prevent elements clashing when window is shrunk too far

---
+++ godot/assets/main/set_min_window_size.gd
@@ -0,0 +1,9 @@
+## Sets the minimum window size of the DisplayServer running the project
+extends Node
+
+const MIN_X: int = 700
+const MIN_Y: int = 360
+
+
+func _ready() -> void:
+    DisplayServer.window_set_min_size(Vector2i(MIN_X, MIN_Y))
