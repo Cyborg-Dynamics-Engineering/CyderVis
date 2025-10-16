6baa432707b953114eb542147d523ee07730bed6
Timothy Mead <t.mead@cyborgdynamicseng.com>
2025-10-16 12:10:26 +1000

    Modify alert handler to only allow one popup alert at a given time by freeing and existing popups when a new one is created

--- godot/assets/alert_handler.gd
+++ godot/assets/alert_handler.gd
@@ -3,9 +3,12 @@ class_name AlertHandler
 
 static var _handler_instance: AlertHandler
 
+var prev_alert_box: AcceptDialog
+
 
 func _ready() -> void:
 	_handler_instance = self
+	prev_alert_box = null
 
 
 static func singleton() -> AlertHandler:
@@ -16,6 +19,10 @@ static func display_error(msg: String) -> void:
 	var handler_ref := singleton()
 	if handler_ref == null:
 		return
+	
+	# Delete any existing alert boxes if present
+	if is_instance_valid(handler_ref.prev_alert_box):
+		handler_ref.prev_alert_box.queue_free()
 
 	var alert_box: AcceptDialog = AcceptDialog.new()
 	alert_box.title = ""
@@ -26,3 +33,5 @@ static func display_error(msg: String) -> void:
 	alert_box.show()
 	alert_box.exclusive = false
 	alert_box.position = Vector2i((_handler_instance.get_viewport().size.x - alert_box.size.x) / 2, (_handler_instance.get_viewport().size.y - alert_box.size.y) - 50)
+
+	handler_ref.prev_alert_box = alert_box
