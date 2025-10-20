c1a433f936d977457ecefe1494a8262e6a4479ca
Timothy Mead <t.mead@cyborgdynamicseng.com>
2025-10-20 14:40:37 +1000

    Fix transmit alert display formatting

--- gdextension_can_io/src/lib.rs
+++ gdextension_can_io/src/lib.rs
@@ -226,8 +226,10 @@ async fn read_can(
             for frame in frames_to_send.clone().into_iter() {
                 if let Err(e) = socket.write_frame(frame).await {
                     error_alert_godot(format!(
-                        "Received error when transmitting frames (Some messages may have not been sent): {e:?}"
+                        "Error when transmitting frames (Some messages may have not been sent): {:?}",
+                        e.to_string()
                     ));
+                    godot_error!("{e:?}");
                     break;
                 }
             }
