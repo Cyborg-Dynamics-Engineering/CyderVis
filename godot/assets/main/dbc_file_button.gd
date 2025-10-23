2355351a994a3d0d2912a5b07eaee74bca5db475
Timothy Mead <t.mead@cyborgdynamicseng.com>
2025-10-23 11:31:12 +1000

    Rework dbc file loading, don't allow dbc modifications while bus is active. Add manual editing of dbc file path

--- godot/assets/main/dbc_file_button.gd
+++ godot/assets/main/dbc_file_button.gd
@@ -3,13 +3,12 @@ class_name DbcFileButton
 
 @export_category("Node References")
 @export var _can_bridge: GodotCanBridge
-@export var _receive_table: ReceiveTable
 @export var _dbc_file_box: LineEdit
-@export var _pause_button: PauseButton
 
 
 func _ready() -> void:
 	self.pressed.connect(_button_pressed)
+
 
 func _button_pressed() -> void:
 	var file_dialog = FileDialog.new()
@@ -20,10 +19,8 @@ func _button_pressed() -> void:
 	file_dialog.popup()
 	file_dialog.file_selected.connect(_process_file)
 
+
 func _process_file(x: String) -> void:
 	var dbc_success = _can_bridge.load_dbc_file(x) # This emits an alert if bad file
-	_dbc_file_box.text = x
 	if dbc_success:
-		# If you attempt, it would be a fail
-		_pause_button.close_connection()
-		_receive_table.clear_all()
+		_dbc_file_box.text = x
