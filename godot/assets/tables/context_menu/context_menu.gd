7de019c7bdee7250f980bd42b7207b0329cf421a
Timothy Mead <t.mead@cyborgdynamicseng.com>
2025-10-24 13:24:16 +1000

    Clamp context menu position to never appear off-screen

--- godot/assets/tables/context_menu/context_menu.gd
+++ godot/assets/tables/context_menu/context_menu.gd
@@ -11,8 +11,14 @@ class_name ContextMenu
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed():
 		var last_mouse_pos = get_global_mouse_position()
-		if not self.get_global_rect().has_point(last_mouse_pos):
-			self.hide()
+		if not get_global_rect().has_point(last_mouse_pos):
+			hide()
+
+
+func _set_position_clamped(new_pos: Vector2):
+	var screen_size = get_viewport_rect().size
+	position.x = clamp(new_pos.x, 0, screen_size.x - size.x)
+	position.y = clamp(new_pos.y, 0, screen_size.y - size.y)
 
 
 # Clears all buttons from the context menu
@@ -27,12 +33,12 @@ func add_item(display_text: String, callback: Callable):
 	var new_button := Button.new()
 	new_button.text = display_text
 	new_button.pressed.connect(callback)
-	new_button.pressed.connect(self.hide)
+	new_button.pressed.connect(hide)
 	button_container.add_child(new_button)
 
 
 # The ContextMenu will resize and show itself at the provided location
 func appear(mouse_location: Vector2):
-	self.size = get_combined_minimum_size()
-	self.position = mouse_location
-	self.show()
+	size = get_combined_minimum_size()
+	_set_position_clamped(mouse_location)
+	show()
